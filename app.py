d0f4bab5cb04ec4c7d4ce833e14cad6d07b28c47
Gabriel-by34 <museradotcom04@gmail.com>
2026-02-18 20:13:22 +0300

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #import flask framework
-from flask import*
+from flask import *
 
 
 # below we create a web server based on application
@@ -23,13 +23,23 @@ def products():
 # Below is route with addition
 @app.route("/api/calc", methods=["POST"])
 def calculator():
-  if request.method==["POST"]:
-    number1=request.form["number1"]
-    number2=request.form["number2"]
-    sum=int(number1)+int(number2)
-    return jsonify({"The answer is" :sum})
+  if request.method=="POST":
+    number1 = request.form["number1"]
+    number2 = request.form["number2"]
+    sum = float(number1)+ float(number2)
+    return jsonify({"The answer is": sum})
 
 
+#Assignment 01
+# create a route that is able to calculate the simple interest given the pricipal as 20000, rate as 7% and time as 8 years.
+@app.route("/api/interest", methods=["POST"])
+def interest():
+    if request.method=="POST":
+     principal= request.form["principal"]
+     rate = request.form["rate"]
+     time = request.form["time"]
+     simple_interest=int(principal)*int(time)*int(rate)/100
+    return jsonify({"The simple interest":simple_interest})
 
 app.run(debug=True)
 
